42fe8ceffac7d16342c61c856065c93162e57f63
wntrrs <winterreissen@gmail.com>
2024-03-07 02:18:44 +0300

    Правки

---
+++ hw-2.js
@@ -0,0 +1,52 @@
+let a;
+a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+let firstIphone = 2007;
+alert(firstIphone);
+
+let creator = 'Brendan Eich';
+alert(creator);
+
+let c = 10;
+let d = 2;
+alert(c + d);
+alert(c - d);
+alert(c * d);
+alert(c / d);
+
+let result = 2 ** 5;
+alert(result);
+
+let e = 9;
+let f = 2;
+let modulo = e % f;
+alert(modulo);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+let age = Number(prompt('Сколько вам лет?'));
+alert(age);
+
+let user = {
+    name: 'abcdef',
+    age: 77,
+    isAdmin: true
+};
+user['city of residence'] = 'Moscow';
+user.age = 20;
+delete user['city of residence'];
+let info = prompt('Какую информацию хотите узнать о пользователе?', 'name');
+alert(user[info]);
+
+let userName = prompt('Как вас зовут?');
+alert(`Привет, ${userName}!`);
